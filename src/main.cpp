bd55b41c129bd1e38b860ae83f6566c498b4f93e
Siraj Abdulkadir <imsirajabdulkadir>
2025-06-29 09:35:15 +0300

    reordering files

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,32 @@
 #include <raylib.h>
-
-int main() 
+#include <stdio.h>
+int main()
 {
+    Image backgroundImage = LoadImage("src/BGs/bg sized.png");
+    if (backgroundImage.data == NULL)
+    {
+        return 1;
+    }
+    int imageWidth = 1366;
+    int imageHeight = 768;
+    int windowWidth = imageWidth;
+    int windowHeight = imageHeight;
 
-};
+    InitWindow(windowWidth, windowHeight, "Fits!");
+
+    Texture2D backgroundTexture = LoadTextureFromImage(backgroundImage);
+    UnloadImage(backgroundImage);
+
+    SetTargetFPS(60);
+    while (WindowShouldClose() == false)
+    {
+
+        BeginDrawing();
+        ClearBackground(WHITE);
+        DrawTexture(backgroundTexture, 0, 0, WHITE);
+        EndDrawing();
+    }
+    UnloadTexture(backgroundTexture);
+    CloseWindow();
+    return 0;
+}
